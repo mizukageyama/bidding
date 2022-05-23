36bdb58828f01fd7ea7c18f3b5e4f9366fa006d5
mizukageyama <emmy2699@gmail.com>
2022-05-23 10:19:04 +0800

    Added: New Firebase Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:bidding/firebase_options.dart';
 import 'package:bidding/routes/app_pages.dart';
 import 'package:bidding/shared/_packages_imports.dart';
-import 'package:bidding/shared/constants/firebase.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'shared/controllers/all_controller_binding.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: firebaseConfig);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- lib/shared/constants/firebase.dart
+++ lib/shared/constants/firebase.dart
@@ -4,11 +4,3 @@ FirebaseFirestore firestore = FirebaseFirestore.instance;
 FirebaseAuth auth = FirebaseAuth.instance;
 FirebaseStorage storage = FirebaseStorage.instance;
 Reference storageRef = storage.ref();
-
-const firebaseConfig = FirebaseOptions(
-    apiKey: "AIzaSyBAkXXsCcfAS3hyRKc-VJt4DSPOZiLMjHY",
-    authDomain: "bidding-4af0e.firebaseapp.com",
-    projectId: "bidding-4af0e",
-    storageBucket: "bidding-4af0e.appspot.com",
-    messagingSenderId: "1016446512811",
-    appId: "1:1016446512811:web:5d4a6ee51295a6787c11ee");
