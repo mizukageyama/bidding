fc53a08210367909b6119f085109a80a3c4e56c2
00black-coder <anya.desmond99@gmail.com>
2022-06-02 19:59:57 +0800

    admin side menu

--- lib/main/admin/controllers/admin_side_menu_controller.dart
+++ lib/main/admin/controllers/admin_side_menu_controller.dart
@@ -1,3 +1,6 @@
+import 'package:bidding/main/admin/controllers/closed_auction_controller.dart';
+import 'package:bidding/main/admin/controllers/open_auction_controller.dart';
+import 'package:bidding/main/admin/controllers/sold_auction_controller.dart';
 import 'package:bidding/models/user_model.dart';
 import 'package:bidding/shared/_packages_imports.dart';
 import 'package:bidding/shared/controllers/_controllers.dart';
@@ -9,9 +12,19 @@ class AdminSideMenuController extends GetxController {
   final UserModel user = _authController.userModel.value!;
   final RxString activeMenu = 'Dashboard'.obs;
 
+  final OpenAuctionController _openAuction =
+      Get.put(OpenAuctionController(), permanent: true);
+  final ClosedAuctionController _closedAuction =
+      Get.put(ClosedAuctionController(), permanent: true);
+  final SoldAuctionController _soldAuction =
+      Get.put(SoldAuctionController(), permanent: true);
+
   @override
   void dispose() {
     Get.delete<AdminSideMenuController>(force: true);
+    Get.delete<OpenAuctionController>(force: true);
+    Get.delete<ClosedAuctionController>(force: true);
+    Get.delete<SoldAuctionController>(force: true);
     super.dispose();
   }
 
